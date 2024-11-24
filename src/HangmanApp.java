5f2e0dbe60329d8f96bf3ed01e97fb017f4cf6d3
gilli <gilli@gillians>
2024-11-23 20:28:10 -1000

    File update 1 - added a comment

---
+++ src/HangmanApp.java
@@ -0,0 +1,27 @@
+/*
+Author: Gillian Harris
+Date: 11/19/2024
+Description: Hangman Game
+*/
+// add a comment of your choice to the top of your code file
+
+
+import java.util.Scanner;
+
+public class HangmanApp {
+
+    public static void main(String[] args) {
+        System.out.println("Play the H A N G M A N game");
+        
+        Scanner sc = new Scanner(System.in);        
+        String again = "y";        
+        while(again.equalsIgnoreCase("y")) {
+            var hangman = new Hangman();
+            hangman.drawHangman(7);
+            hangman.playGame();
+            System.out.println();
+            System.out.print("Play again? (y/n): ");
+            again = sc.nextLine();
+        }
+    } 
+}
